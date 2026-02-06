e43baa1ef56007c4e61423a2ed5da01f5ac38a01
Lucas-schnkl <lucas.schenkel12@gmail.com>
2026-02-06 18:41:33 +0100

    Ex 6.1 (affichage réponse json)

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,17 @@ import type { Request, Response } from 'express';
 
 const  app = express();
 const port = 3000;
+const route = "/api/data"
+const etudiants = [
+{ id: 1, nom: "Dupont", prenom: "Jean" },
+{ id: 2, nom: "Martin", prenom: "Sophie" },
+{ id: 3, nom: "Doe", prenom: "John" },
+];
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Bienvenue sur mon serveur API');
+app.get(`${route}`, (req: Request, res: Response) => {
+    res.json(`${JSON.stringify(etudiants)}`);
 });
 
 app.listen(port, () => {
-    console.log(`Serveur sur http://localhost:${port}`);
+    console.log(`Serveur sur http://localhost:${port}${route}`);
 });
