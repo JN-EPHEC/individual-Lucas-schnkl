c5885e29fee6080eef4d40f9f416fc90a54d02dd
Lucas-schnkl <lucas.schenkel12@gmail.com>
2026-02-06 18:58:14 +0100

    Ex 6.2 (paramètres dans la route)

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import type { Request, Response } from 'express';
 
 const  app = express();
 const port = 3000;
-const route = "/api/data"
+const route = "/api/hello/:name"
+
 const etudiants = [
 { id: 1, nom: "Dupont", prenom: "Jean" },
 { id: 2, nom: "Martin", prenom: "Sophie" },
 { id: 3, nom: "Doe", prenom: "John" },
 ];
+const date_ajd = new Date();
 
 app.get(`${route}`, (req: Request, res: Response) => {
-    res.json(`${JSON.stringify(etudiants)}`);
+    res.send(` {"message": "Bonjour ${req.params['name']}", "timestamp" : "${date_ajd.toLocaleDateString()}" }`);
 });
 
 app.listen(port, () => {
