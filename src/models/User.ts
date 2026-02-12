e4153588b6884a23faad9a4d6d64cfdf446c9c1e
Lucas-schnkl <lucas.schenkel12@gmail.com>
2026-02-12 15:29:48 +0100

    Ajout modèle

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+class User extends Model {}
+
+User.init(
+  {
+    // Model attributes are defined here
+    prenom: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nom: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  },
+  {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'User', // We need to choose the model name
+  },
+);
