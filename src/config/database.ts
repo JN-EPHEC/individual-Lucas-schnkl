5c9d4203f3b46a427faaac214e29edfa83521f42
Lucas-schnkl <lucas.schenkel12@gmail.com>
2026-02-13 18:21:03 +0100

    Rien changé mais fonctionne mtn

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 const seq = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite'
+  storage: './database.sqlite' //Par rapport à où on lance node
 });
 
 export default seq;
