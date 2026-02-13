5ba129828f6ed591e8113e2cd9dd1872e76a4758
Lucas-schnkl <lucas.schenkel12@gmail.com>
2026-02-13 21:44:10 +0100

    3 Refactoring de l’API

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,26 @@
 import {Router, type Request, type Response} from 'express';
+import User from '../models/User.js';
 
 const router = Router();
+const date_ajd = new Date();
 
-const users = [
-{ id: 1, name: "Alice" },
-{ id: 2, name: "Bob" },
-];
+router.get('/', async function(req: Request, res: Response) {    
+    let users = await User.findAll();
+    res.send(users);
+});
 
-router.get('/', (req: Request, res: Response) => {
-    res.json(users);
+router.post('/', (req: Request, res: Response) =>{
+    User.create({nom : "testNom", prenom : "testPrenom", createdAt : date_ajd.toLocaleDateString(), updatedAt : date_ajd.toLocaleDateString()});
+    res.send((req.body));
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    User.destroy({
+        where: {
+            id: req.params['id']
+        }
+    });
+    res.send(req.body);
 });
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ import userRouter from './routes/userRoutes.js';
 import sequelize from './config/database.js';
 import User from './models/User.js';
 
-const  app = express();
+const app = express();
 const port = 3000;
 const route = "/api/hello/:name"
 
+app.use(express.json());
 
 const etudiants = [
 { id: 1, nom: "Dupont", prenom: "Jean" },
@@ -20,15 +21,11 @@ app.get(`${route}`, (req: Request, res: Response) => {
     res.send(` {"message": "Bonjour ${req.params['name']}", "timestamp" : "${date_ajd.toLocaleDateString()}" }`);
 });
 
-app.listen(port, () => {
-    console.log(`Serveur sur http://localhost:${port}${route}`);
-});
-
 app.use('/api/users', userRouter);
 
 sequelize.sync().then(() => {
     console.log("Base de données synchronisée");
     app.listen(port, () =>{
-        console.log("Serveur ok");
+        console.log(`Serveur ok http://localhost:${port}${route}`);
     });
 });
