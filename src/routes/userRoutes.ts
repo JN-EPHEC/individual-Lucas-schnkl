1042945a3242f22df3e49c298754c6fa5825364a
Lucas-schnkl <lucas.schenkel12@gmail.com>
2026-02-17 20:45:14 +0100

    Correction code + ajout gestion erreurs

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,22 +5,38 @@ const router = Router();
 const date_ajd = new Date();
 
 router.get('/', async function(req: Request, res: Response) {    
-    let users = await User.findAll();
-    res.send(users);
+    try{
+        let users = await User.findAll();
+        res.status(200).json(users);
+    } catch (error){
+        console.error(error);
+        res.status(500).send("Erreur serveur");
+    }
 });
 
 router.post('/', async (req: Request, res: Response) =>{
-    await User.create({nom : req.body.nom, prenom : req.body.prenom, createdAt : date_ajd.toLocaleDateString(), updatedAt : date_ajd.toLocaleDateString()});
-    res.send((req.body));
+    try{
+        await User.create({nom : req.body.nom, prenom : req.body.prenom, createdAt : date_ajd.toLocaleDateString(), updatedAt : date_ajd.toLocaleDateString()});
+        res.status(201).json(req.body);
+    } catch (error){
+        console.log(error);
+        res.status(500).send("Erreur serveur");
+    }
 });
 
 router.delete('/:id', async (req: Request, res: Response) => {
-    await User.destroy({
-        where: {
-            id: req.params['id']
-        }
-    });
-    res.send(req.body);
+    try{
+        await User.destroy({
+            where: {
+                id: req.params['id']
+            }
+        });
+        res.status(204).json(req.body);
+
+    }catch (error){
+        console.log(error);
+        res.status(500).send("Erreur serveur");
+    }
 });
 
 export default router;
